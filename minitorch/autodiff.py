3201237a48f930050562802d20cc1af356f0f602
Amal S Sebastian <amalssebastian@gmail.com>
2024-09-23 23:39:24 -0400

    Task 1_2 done

--- minitorch/autodiff.py
+++ minitorch/autodiff.py
@@ -22,8 +22,12 @@ def central_difference(f: Any, *vals: Any, arg: int = 0, epsilon: float = 1e-6) 
     Returns:
         An approximation of $f'_i(x_0, \ldots, x_{n-1})$
     """
-    # TODO: Implement for Task 1.1.
-    raise NotImplementedError('Need to implement for Task 1.1')
+    print(vals)
+    x_plus, x_minus = list(vals), list(vals)
+    x_plus[arg] = x_plus[arg] + epsilon
+    x_minus[arg] = x_minus[arg] - epsilon
+
+    return (f(*x_plus) - f(*x_minus)) / (2 * epsilon)
 
 
 variable_count = 1
@@ -62,7 +66,7 @@ def topological_sort(variable: Variable) -> Iterable[Variable]:
         Non-constant Variables in topological order starting from the right.
     """
     # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    raise NotImplementedError("Need to implement for Task 1.4")
 
 
 def backpropagate(variable: Variable, deriv: Any) -> None:
@@ -77,7 +81,7 @@ def backpropagate(variable: Variable, deriv: Any) -> None:
     No return. Should write to its results to the derivative values of each leaf through `accumulate_derivative`.
     """
     # TODO: Implement for Task 1.4.
-    raise NotImplementedError('Need to implement for Task 1.4')
+    raise NotImplementedError("Need to implement for Task 1.4")
 
 
 @dataclass
